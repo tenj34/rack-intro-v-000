41e7ac254b9aebaee544d42ed692aa1a9ab01884
Juan Tenezaca <tenj34@gmail.com>
2018-10-13 18:07:48 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World my name is Juan Tenezaca"
+    resp.write "Hello, my name is Juan Tenezaca"
     resp.finish
   end
 
